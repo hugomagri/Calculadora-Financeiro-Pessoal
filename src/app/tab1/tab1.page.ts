d52c4b5fd40fbb9dff1e1c6bd1c679742f11ad7d
hugo magri <hugomagrimariutti07@gmail.com>
2025-06-08 22:24:13 -0300

    dia 2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,12 +8,43 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class Tab1Page {
-  isCardVisible: boolean = false; // Inicialmente o card está oculto
+  valor: number = 0;
+  taxa: number = 0;
+  meses: number = 0;
+  resultado: number = 0;
+  isCardVisible: boolean = false;
 
-  toggleCard() {
-    this.isCardVisible = !this.isCardVisible; // Alterna entre mostrar e ocultar o card
+  constructor(private alertController: AlertController) {}
+
+  async calcularEmprestimo() {
+    const V = this.valor;
+    const i = this.taxa; // taxa mensal
+    const n = this.meses;
+
+    if (V <= 0 || this.taxa <= 0 || n <= 0) {
+      this.isCardVisible = false;
+      await this.showAlert('Erro', 'Por favor, preencha todos os campos com valores válidos.');
+      return;
+    }
+
+    if (this.taxa > 20) {
+      await this.showAlert('Atenção', 'Taxa de juros acima de 20% ao ano. Este empréstimo pode ser arriscado!');
+    } else {
+      await this.showAlert('Aviso', 'Considere avaliar com cuidado as condições do empréstimo.');
+    }
+
+    const parcela = (V * (i/100))/(1-(1+(i/100)**-n));
+    this.resultado = parcela;
+    this.isCardVisible = true;
   }
 
-  constructor() {}
-
+  async showAlert(titulo: string, mensagem: string) {
+    const alert = await this.alertController.create({
+      header: titulo,
+      message: mensagem,
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
 }
+
